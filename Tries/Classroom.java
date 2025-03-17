99ad5164274cd85097fe9d3b9fde4a18868b69c7
LungsomLamnio1004 <lungsom.lamnio.adtu@gmail.com>
2025-03-17 11:36:44 +0530

    Tries folder added

---
+++ Tries/Classroom.java
@@ -0,0 +1,19 @@
+package Tries;
+
+public class Classroom {
+    static class Node {
+        Node children[] = new Node[26];
+        boolean eow = false;
+
+        Node() {
+            for(int i=0; i<children.length; i++) {
+                children[i] = null;
+            }
+        }
+    }
+
+    public static Node root = new Node();
+    public static void main(String[] args) {
+        
+    }
+}
