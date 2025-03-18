88c35647fb523de51879b77f193a9af5843c88af
LungsomLamnio1004 <lungsom.lamnio.adtu@gmail.com>
2025-03-18 22:02:21 +0530

    insert function created

--- Tries/Classroom.java
+++ Tries/Classroom.java
@@ -13,7 +13,24 @@ public class Classroom {
     }
 
     public static Node root = new Node();
+
+    public static void insert(String word) {
+        Node curr = root;
+        for(int level=0; level<word.length(); level++) {
+            int indx = word.charAt(level) - 'a';
+            if(curr.children[indx] == null) {
+                curr.children[indx] = new Node();
+            }
+            curr = curr.children[indx];
+        }
+
+        curr.eow = true;
+    }
     public static void main(String[] args) {
         String words[] = {"the", "a", "there", "their", "any", "thee"};
+
+        for(int i=0; i<words.length; i++) {
+            insert(words[i]);
+        }
     }
 }
