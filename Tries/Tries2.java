0d0e0bb80f00586851a4588b3a3a61edb431db9e
LungsomLamnio1004 <lungsomlamnio@gmail.com>
2025-03-24 17:47:37 +0530

    longest word with all prefixes

--- Tries/Tries2.java
+++ Tries/Tries2.java
@@ -68,13 +68,36 @@ public class Tries2 {
 
         return count + 1;
     }
-    public static void main(String[] args) {
-        String str = "ababa";
 
-        for(int i=0; i<str.length(); i++) {
-            insert(str.substring(i));
+    public static String ans = "";
+
+    public static void longestWord(Node root, StringBuilder temp) {
+        if(root == null) {
+            return;
         }
 
-        System.out.println(countNodes(root));
+        for(int i=0; i<26; i++) {
+            if(root.children[i] != null && root.children[i].eow == true) {
+                char ch = (char)(i+'a');
+                temp.append(ch);
+
+                if(temp.length() > ans.length()) {
+                    ans = temp.toString();
+                }
+
+                longestWord(root.children[i], temp);
+                temp.deleteCharAt(temp.length()-1);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        String words[] = {"a", "banana", "app", "appl", "ap", "apply", "apple"};
+
+        for(int i=0; i<words.length; i++) {
+            insert(words[i]);
+        }
+
+        longestWord(root, new StringBuilder(""));
+        System.out.println(ans);
     }
 }
