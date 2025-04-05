4b62649dd218f75c3de23de3840ee8d41363ef28
LungsomLamnio1004 <lungsomlamnio@gmail.com>
2025-04-05 12:56:27 +0530

    Graph revision

---
+++ Graphs/GraphRevision.java
@@ -0,0 +1,33 @@
+package Graphs;
+
+import java.util.ArrayList;
+
+public class GraphRevision {
+    static class Edge {
+        int src;
+        int dest;
+        int wt;
+
+        public Edge(int src, int dest, int wt) {
+            this.src = src;
+            this.dest = dest;
+            this.wt = wt;
+        }
+    }
+
+    public static void createGraph(ArrayList<Edge> graph[], int src, int dest, int wt) {
+        for(int i=0; i<graph.length; i++) {
+            for(int j=0; j<graph[i].size(); j++) {
+                Edge e = graph[i].get(j);
+                e.src = src;
+                e.dest = dest;
+                e.wt = wt;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int V = 7;
+        ArrayList<Edge> graph[] = new ArrayList[V];
+    }
+}
