26e808b0cd7bcb6a4190eb83435ae3cff8834ea2
LungsomLamnio1004 <lungsomlamnio@gmail.com>
2025-04-28 14:21:27 +0530

    graph revision

---
+++ Graphs/Classroom.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Classroom {
+    static class Edge {
+        int src;
+        int dest;
+        int weight;
+
+        public Edge(int src, int dest, int weight) {
+            this.src = src;
+            this.dest = dest;
+            this.weight = weight;
+        }
+    }
+    public static void main(String[] args) {
+        int V = 5;
+        ArrayList<Edge> graph[] = new ArrayList[V];
+        System.out.println("Hello");
+    }
+}
