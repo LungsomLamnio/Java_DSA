5901288b9c148829463bb25ee047cf2c450c7609
LungsomLamnio1004 <lungsomlamnio@gmail.com>
2025-05-05 10:45:30 +0530

    bfs revision

--- Graphs/GraphRevision.java
+++ Graphs/GraphRevision.java
@@ -1,7 +1,6 @@
-
-
-import java.util.ArrayList; //importing libraries
-
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.LinkedList;
 public class GraphRevision {
     static class Edge {
         int src;
@@ -15,19 +14,46 @@ public class GraphRevision {
         }
     }
 
-    public static void createGraph(ArrayList<Edge> graph[], int src, int dest, int wt) {
-        for(int i=0; i<graph.length; i++) {
-            for(int j=0; j<graph[i].size(); j++) {
-                Edge e = graph[i].get(j);
-                e.src = src;
-                e.dest = dest;
-                e.wt = wt;
+    public static void bfs(ArrayList<Edge> graph[]) {
+        Queue<Integer> q = new LinkedList<>();
+        boolean isVisited[] = new boolean[graph.length];
+
+        q.add(0);
+        while(!q.isEmpty()) {
+            int curr = q.remove();
+            if(!isVisited[curr]) {
+                isVisited[curr] = true;
+                System.out.print(curr + " ");
+                for(int i=0; i<graph[curr].size(); i++) {
+                    Edge e = graph[curr].get(i);
+                    q.add(e.dest);
+                }
             }
         }
     }
-
     public static void main(String[] args) {
-        int V = 7;
+        int V = 5;
+
         ArrayList<Edge> graph[] = new ArrayList[V];
+        for(int i=0; i<graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[0].add(new Edge(0, 1, 5));
+
+        graph[1].add(new Edge(1, 0, 5));
+        graph[1].add(new Edge(1, 2, 1));
+        graph[1].add(new Edge(1, 3, 3));
+
+        graph[2].add(new Edge(2, 1, 1));
+        graph[2].add(new Edge(2, 3, 1));
+        graph[2].add(new Edge(2, 4, 2));
+
+        graph[3].add(new Edge(3, 1, 3));
+        graph[3].add(new Edge(3, 2, 1));
+
+        graph[4].add(new Edge(4, 2, 2));
+
+        bfs(graph);
     }
 }
