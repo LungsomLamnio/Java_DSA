4f7d66360f4ce73c31b1ca2d147a3ffe5de32792
LungsomLamnio1004 <lungsomlamnio@gmail.com>
2025-07-06 15:48:40 +0530

    bfs and dfs revision done

--- Graphs/CycleDetect.java
+++ Graphs/CycleDetect.java
@@ -19,7 +19,7 @@ public class CycleDetect {
         }
 
         graph[0].add(new Edge(0, 1));
-        graph[0].add(new Edge(0, 4));
+        graph[0].add(new Edge(0, 2));
         
         graph[1].add(new Edge(1, 0));
         graph[1].add(new Edge(1, 3));
@@ -29,9 +29,15 @@ public class CycleDetect {
 
         graph[3].add(new Edge(3, 1));
         graph[3].add(new Edge(3, 4));
+        graph[3].add(new Edge(3, 5));
 
         graph[4].add(new Edge(4, 2));
         graph[4].add(new Edge(4, 3));
+        graph[4].add(new Edge(4, 5));
+
+        graph[5].add(new Edge(5, 6));
+
+        graph[6].add(new Edge(6, 5));
     }
 
     public static boolean detectCycle(ArrayList<Edge> graph[]) {
@@ -66,36 +72,36 @@ public class CycleDetect {
         return false;
     }
 
-    public static boolean isBipartite(ArrayList<Edge> graph[]) {
-        int color[] = new int[graph.length];
-        for(int i=0; i<graph.length; i++) {
-            color[i] = -1;
-        }
+    // public static boolean isBipartite(ArrayList<Edge> graph[]) {
+    //     int color[] = new int[graph.length];
+    //     for(int i=0; i<graph.length; i++) {
+    //         color[i] = -1;
+    //     }
 
-        Queue<Integer> q = new LinkedList<>();
-        for(int i=0; i<graph.length; i++) {
-            if(color[i] == -1) {
-                q.add(0);
-                color[i] = 0;
+    //     Queue<Integer> q = new LinkedList<>();
+    //     for(int i=0; i<graph.length; i++) {
+    //         if(color[i] == -1) {
+    //             q.add(i);
+    //             color[i] = 0;
 
-                while(!q.isEmpty()) {
-                    int curr = q.remove();
-                    for(int j=0; j<graph[curr].size(); j++) {
-                        Edge e = graph[curr].get(j);
+    //             while(!q.isEmpty()) {
+    //                 int curr = q.remove();
+    //                 for(int j=0; j<graph[curr].size(); j++) {
+    //                     Edge e = graph[curr].get(j);
 
-                        if(color[e.dest] == -1) {
-                            int nextColor = color[curr] == 0 ? 1 : 0;
-                            color[e.dest] = nextColor;
-                            q.add(e.dest);
-                        } else if(color[curr] == color[e.dest]) {
-                            return false;
-                        }
-                    }
-                }
-            }
-        }
-        return true;
-    }
+    //                     if(color[e.dest] == -1) {
+    //                         int nextColor = color[curr] == 0 ? 1 : 0;
+    //                         color[e.dest] = nextColor;
+    //                         q.add(e.dest);
+    //                     } else if(color[curr] == color[e.dest]) {
+    //                         return false;
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     return true;
+    // }
 
     public static boolean detectCycleInUndirectedGraph(ArrayList<Edge> graph[]) {
         boolean isVisited[] = new boolean[graph.length];
@@ -128,10 +134,91 @@ public class CycleDetect {
         return false;
     }
 
+    public static boolean isBipartite(ArrayList<Edge> graph[]) {
+        int color[] = new int[graph.length];
+
+        for(int i=0; i<color.length; i++) {
+            color[i] = -1;
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0; i<graph.length; i++) {
+            if(color[i] == -1) {
+                color[i] = 0;
+                q.add(0);
+        
+                while(!q.isEmpty()) {
+                    int curr = q.remove();
+                    for(int j=0; j<graph[curr].size(); j++) {
+                        Edge e = graph[curr].get(j);
+                        
+                        if(color[e.dest] == -1) {
+                            int nextColor = color[curr] == 0 ? 1 : 0;
+                            color[e.dest] = nextColor;
+                            q.add(e.dest);
+                        } else if(color[e.dest] == color[curr]) {
+                            return false;
+                        }
+                    }
+                }
+                
+            }
+        }
+        return true;
+    }
+
+    public static void bfs(ArrayList<Edge> graph[]) {
+        boolean isVisited[] = new boolean[graph.length];
+        for(int i=0; i<graph.length; i++) {
+            if(!isVisited[i]) {
+                bfsUtil(graph, isVisited);
+            }
+        }
+    }
+
+    public static void bfsUtil(ArrayList<Edge> graph[], boolean isVisited[]) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(0);
+
+        while(!q.isEmpty()) {
+            int curr = q.remove();
+            if(!isVisited[curr]) {
+                System.out.print(curr + " ");
+                isVisited[curr] = true;
+            }
+            for(int i=0; i<graph[curr].size(); i++) {
+                Edge e = graph[curr].get(i);
+                q.add(e.dest);
+            }
+        }
+    }
+
+    public static void dfs(ArrayList<Edge> graph[]) {
+        boolean isVisited[] = new boolean[graph.length];
+
+        for(int i=0; i<graph.length; i++) {
+            if(!isVisited[i]) {
+                dfsUtil(graph, isVisited, i);
+            }
+        }
+    }
+
+    public static void dfsUtil(ArrayList<Edge> graph[], boolean isVisited[], int curr) {
+        isVisited[curr] = true;
+        System.out.print(curr + " ");
+
+        for(int i=0; i<graph[curr].size(); i++) {
+            Edge e = graph[curr].get(i);
+            if(!isVisited[e.dest]) {
+                dfsUtil(graph, isVisited, e.dest);
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        int V = 5;
+        int V = 7;
         ArrayList<Edge> graph[] = new ArrayList[V];
         createGraph(graph);
-        System.out.println(detectCycle(graph));
+        dfs(graph);
     }
 }
