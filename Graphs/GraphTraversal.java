bf81fec308d21c1c50c2e67e21ce2f062dc02d3a
LungsomLamnio1004 <lungsomlamnio@gmail.com>
2025-08-16 19:58:13 +0530

    cycle detection in directed graph

--- Graphs/GraphTraversal.java
+++ Graphs/GraphTraversal.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 public class GraphTraversal {
     static class Edge {
@@ -18,20 +19,20 @@ public class GraphTraversal {
             graph[i] = new ArrayList<>();
         }
 
-        graph[0].add(new Edge(0, 1));
+        // graph[0].add(new Edge(0, 1));
         graph[0].add(new Edge(0, 2));
 
         graph[1].add(new Edge(1, 0));
-        graph[1].add(new Edge(1, 3));
+        // graph[1].add(new Edge(1, 3));
 
-        graph[2].add(new Edge(2, 0));
-        graph[2].add(new Edge(2, 4));
+        graph[2].add(new Edge(2, 3));
+        // graph[2].add(new Edge(2, 4));
 
-        graph[3].add(new Edge(3, 1));
-        graph[3].add(new Edge(3, 4));
+        graph[3].add(new Edge(3, 0));
+        // graph[3].add(new Edge(3, 4));
 
-        graph[4].add(new Edge(4, 2));
-        graph[4].add(new Edge(4, 3));
+        // graph[4].add(new Edge(4, 2));
+        // graph[4].add(new Edge(4, 3));
         // graph[3].add(new Edge(3, 5));
 
         // graph[4].add(new Edge(4, 2));
@@ -210,10 +211,43 @@ public class GraphTraversal {
         return true;
      }
 
+     public static boolean detectCycleDirected(ArrayList<Edge> graph[]) {
+        boolean isVisited[] = new boolean[graph.length];
+        boolean stack[] = new boolean[graph.length];
+
+        for(int i=0; i<graph.length; i++) {
+            if(!isVisited[i]) {
+                if(detectCycleDirectedUtil(graph, i, isVisited, stack)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+     }
+
+     public static boolean detectCycleDirectedUtil(ArrayList<Edge> graph[], int curr, boolean isVisited[], boolean stack[]) {
+        isVisited[curr] = true;
+        stack[curr] = true;
+
+        for(int i=0; i<graph[curr].size(); i++) {
+            Edge e = graph[curr].get(i);
+
+            if(stack[e.dest]) {
+                return true;
+            } else if(!isVisited[e.dest]) {
+                if(detectCycleDirectedUtil(graph, e.dest, isVisited, stack)) {
+                    return true;
+                }
+            }
+        }
+        stack[curr] = false;
+        return false;
+     }
+
     public static void main(String[] args) {
         int V = 7;
         ArrayList<Edge> graph[] = new ArrayList[V];
         createGraph(graph);
-        System.out.println(isBipartite(graph));
+        System.out.println(detectCycleDirected(graph));
     }
 }
