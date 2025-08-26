a13475cc8d65622ac7f077419ee7f9a08124f6fb
LungsomLamnio1004 <lungsomlamnio@gmail.com>
2025-08-26 23:21:33 +0530

    dijkstra algo revision done

--- Graphs/GraphTraversal.java
+++ Graphs/GraphTraversal.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Stack;
 
@@ -7,10 +8,12 @@ public class GraphTraversal {
     static class Edge {
         int src;
         int dest;
+        int wt;
 
-        public Edge(int src, int dest) {
+        public Edge(int src, int dest, int wt) {
             this.src = src;
             this.dest = dest;
+            this.wt = wt;
         }
     }
 
@@ -19,28 +22,29 @@ public class GraphTraversal {
             graph[i] = new ArrayList<>();
         }
 
-        // graph[0].add(new Edge(0, 1));
-        graph[0].add(new Edge(0, 3));
+        graph[0].add(new Edge(0, 1, 2));
+        graph[0].add(new Edge(0, 2, 4));
 
         // graph[1].add(new Edge(1, 0));
-        // graph[1].add(new Edge(1, 3));
+        graph[1].add(new Edge(1, 2, 1));
+        graph[1].add(new Edge(1, 3, 7));
 
-        graph[2].add(new Edge(2, 3));
-        // graph[2].add(new Edge(2, 4));
+        // graph[2].add(new Edge(2, 3));
+        graph[2].add(new Edge(2, 4, 3));
 
-        graph[3].add(new Edge(3, 1));
+        graph[3].add(new Edge(3, 5, 1));
         // graph[3].add(new Edge(3, 4));
 
-        graph[4].add(new Edge(4, 0));
-        graph[4].add(new Edge(4, 1));
+        graph[4].add(new Edge(4, 3, 2));
+        graph[4].add(new Edge(4, 5, 5));
         // graph[3].add(new Edge(3, 5));
 
         // graph[4].add(new Edge(4, 2));
         // graph[4].add(new Edge(4, 3));
         // graph[4].add(new Edge(4, 5));
 
-        graph[5].add(new Edge(5, 0));
-        graph[5].add(new Edge(5, 2));
+        // graph[5].add(new Edge(5, 0));
+        // graph[5].add(new Edge(5, 2));
         // graph[5].add(new Edge(5, 6));
 
         // graph[6].add(new Edge(6, 5));
@@ -317,10 +321,64 @@ public class GraphTraversal {
         }
     }
 
+    static class Pair implements Comparable<Pair>{
+        int node;
+        int path;
+
+        public Pair(int node, int path) {
+            this.node = node;
+            this.path = path;
+        }
+
+        @Override
+        public int compareTo(Pair p2) {
+            return this.path - p2.path;
+        }
+    }
+
+    public static void dijkstra(ArrayList<Edge> graph[], int src) {
+        int dist[] = new int[graph.length];
+        for(int i=0; i<graph.length; i++) {
+            if(i != src) {
+                dist[i] = Integer.MAX_VALUE;
+            }
+        }
+
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        boolean isVisited[] = new boolean[graph.length];
+        pq.add(new Pair(src, 0));
+
+        while(!pq.isEmpty()) {
+            Pair curr = pq.remove();
+
+            if(!isVisited[curr.node]) {
+                isVisited[curr.node] = true;
+
+                for(int i=0; i<graph[curr.node].size(); i++) {
+                    Edge e = graph[curr.node].get(i);
+
+                    int u = e.src;
+                    int v = e.dest;
+                    int wt = e.wt;
+
+                    if(dist[u] + wt < dist[v]) {
+                        dist[v] = dist[u] + wt;
+                        pq.add(new Pair(v, dist[v]));
+                    }
+                }
+            }
+        }
+
+        for(int i=0; i<dist.length; i++) {
+            System.out.print(dist[i] + " ");
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         int V = 6;
         ArrayList<Edge> graph[] = new ArrayList[V];
         createGraph(graph);
-        allPaths(graph, 5, 1, new String(""));
+        dijkstra(graph, 0);
     }
 }
