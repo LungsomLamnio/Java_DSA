c023f007657178720d50323a5b4a05fc33363625
LungsomLamnio1004 <lungsomlamnio@gmail.com>
2025-10-02 15:02:02 +0530

    fibonacci series using DP

--- DP/Fibonacci.java
+++ DP/Fibonacci.java
@@ -1,13 +1,19 @@
 class Fibonacci {
-    public static int findFibonacci(int n) {
+    public static int findFibonacci(int n, int f[]) {
         if(n == 0 || n == 1) {
             return n;
         }
 
-        return findFibonacci(n-1) + findFibonacci(n-2);
+        if(f[n] != 0) {
+            return f[n];
+        }
+
+        f[n] = findFibonacci(n-1, f) + findFibonacci(n-2, f);
+        return f[n];
     }
     public static void main(String[] args) {
         int n = 5;
-        System.out.println(findFibonacci(n));
+        int f[] = new int[n+1];
+        System.out.println(findFibonacci(n, f));
     }
 }
