eec43a5a448e39477ea9989e4d445eac2b13bac1
LungsomLamnio1004 <lungsomlamnio@gmail.com>
2025-11-22 19:26:15 +0530

    climbing stairs problem using tabular format

--- DP/Revision.java
+++ DP/Revision.java
@@ -17,10 +17,21 @@ public class Revision {
         return climbingStairsUsingMemoization(ways, n-1) + climbingStairsUsingMemoization(ways, n-2);
     }
 
+    public static int climbingStairsUsingTabular(int n) {
+        int dp[] = new int[n+1];
+        dp[0] = dp[1] = 1;
+
+        for(int i=2; i<=n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
+    }
+
     public static void main(String[] args) {
         int n = 5;
         int ways[] = new int[n+1];
         Arrays.fill(ways, -1);
-        System.out.println(climbingStairsUsingMemoization(ways, n));
+        System.out.println("Tab: " + climbingStairsUsingTabular(n));
     }
 }
