3a07338672535c6e8541b840d56dba185dd32e27
LungsomLamnio1004 <lungsomlamnio@gmail.com>
2025-12-02 23:08:53 +0530

    target sum subset using tabulation done

--- DP/KnapsackRevision.java
+++ DP/KnapsackRevision.java
@@ -67,11 +67,44 @@ class KnapsackRevision {
 
         return dp[n][W];
     }
+
+    public static void printDp(boolean dp[][]) {
+        for(int i=0; i<dp.length; i++) {
+            for(int j=0; j<dp[0].length; j++) {
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static boolean targetSumSubset(int arr[], int sum) {
+        int n = arr.length;
+        boolean dp[][] = new boolean[n+1][sum+1];
+
+        //sum is zero for all the items
+        for(int i=0; i<dp.length; i++) {
+            dp[i][0] = true;
+        }
+
+        for(int i=1; i<n+1; i++) {
+            for(int j=1; j<sum+1; j++) {
+                int v = arr[i-1];
+                if(v <= j && dp[i-1][j-v] == true) {
+                    dp[i][j] = true;
+                } else if(dp[i-1][j] == true) {
+                    dp[i][j] = true;
+                }
+            }
+        }
+
+        printDp(dp);
+        return dp[n][sum];
+    }
     
     public static void main(String[] args) {
-        int val[] = {15, 14, 10, 45, 30};
-        int wt[] = {2, 5, 1, 3, 4};
-        int W = 7;
-        System.out.println(knapsackTabulation(val, wt, W));
+        int arr[] = {4, 2, 7, 1, 3};
+        int sum = 10;
+        System.out.println(targetSumSubset(arr, sum));
     }
 }
