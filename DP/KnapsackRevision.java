ba78ebff09ef6d663694b074b722ff3251e91be3
LungsomLamnio1004 <lungsomlamnio@gmail.com>
2025-12-03 14:21:31 +0530

    coin change using tabulation

--- DP/KnapsackRevision.java
+++ DP/KnapsackRevision.java
@@ -127,11 +127,32 @@ class KnapsackRevision {
 
         return dp[n][W];
     }
+
+    public static int coinChange(int coins[], int sum) {
+        int n = coins.length;
+        int dp[][] = new int[n+1][sum+1];
+
+        //0th col initialization
+        for(int i=0; i<dp.length; i++) {
+            dp[i][0] = 1;
+        }
+
+        for(int i=1; i<n+1; i++) {
+            for(int j=1; j<sum+1; j++) {
+                if(coins[i-1] <= j) {
+                    dp[i][j] = dp[i][j-coins[i-1]] + dp[i-1][j];
+                } else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+
+        return dp[n][sum];
+    }
     
     public static void main(String[] args) {
-        int val[] = {15, 14, 10, 45, 30};
-        int wt[] = {2, 5, 1, 3, 4};
-        int W = 7;
-        System.out.println(unboundedKnapsack(val, wt, W));
+        int coins[] = {2, 5, 3, 6};
+        int sum = 10;
+        System.out.println(coinChange(coins, sum));
     }
 }
