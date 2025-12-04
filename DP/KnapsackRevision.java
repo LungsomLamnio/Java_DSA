35250738d1f627a8a3ba257f3f0945cfab34fa48
LungsomLamnio1004 <lungsomlamnio@gmail.com>
2025-12-04 19:22:39 +0530

    lcs problem solved using all recursion, memoization and tabulation

--- DP/KnapsackRevision.java
+++ DP/KnapsackRevision.java
@@ -174,11 +174,67 @@ class KnapsackRevision {
 
         return dp[n][rodLength];
     }
+
+    public static int lcs(String str1, String str2, int m, int n) {
+        if(m == 0 || n == 0) {
+            return 0;
+        }
+
+        if(str1.charAt(m-1) == str2.charAt(n-1)) {
+            return lcs(str1, str2, m-1, n-1) + 1;
+        } else {
+            int ans1 = lcs(str1, str2, m-1, n);
+            int ans2 = lcs(str1, str2, m, n-1);
+            return Math.max(ans1, ans2);
+        }
+    }
+
+    public static int lcsMemo(String str1, String str2, int m, int n, int dp[][]) {
+        if(m == 0 || n == 0) {
+            return 0;
+        }
+
+        if(dp[m][n] != -1) {
+            return dp[m][n];
+        }
+
+        if(str1.charAt(m-1) == str2.charAt(n-1)) {
+            return dp[m][n] = lcsMemo(str1, str2, m-1, n-1, dp) + 1;
+        } else {
+            int ans1 = lcsMemo(str1, str2, m-1, n, dp);
+            int ans2 = lcsMemo(str1, str2, m, n-1, dp);
+            return dp[m][n] = Math.max(ans1, ans2);
+        }
+    }
+
+    public static int lcsTab(String str1, String str2) {
+        int m = str1.length();
+        int n = str2.length();
+        int dp[][] = new int[m+1][n+1];
+
+        for(int i=0; i<dp.length; i++) {
+            dp[i][0] = 0;
+        }
+        for(int i=0; i<dp[0].length; i++) {
+            dp[0][i] = 0;
+        }
+
+        for(int i=1; i<m+1; i++) {
+            for(int j=1; j<n+1; j++) {
+                if(str1.charAt(i-1) == str2.charAt(j-1)) {
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
     
     public static void main(String[] args) {
-        int pieces[] = {1, 2, 3, 4, 5, 6, 7, 8};
-        int price[] = {1, 5, 8, 9, 10, 17, 17, 20};
-        int rodLength = 8;
-        System.out.println(rodCutting(pieces, price, rodLength));
+        String str1 = "abcdge";
+        String str2 = "abedg";
+        System.out.println(lcsTab(str1, str2));
     }
 }
