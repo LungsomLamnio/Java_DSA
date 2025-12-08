9d8657c0928d2d1ff8dc3e2714b045531995e27e
LungsomLamnio1004 <lungsomlamnio@gmail.com>
2025-12-08 13:36:12 +0530

    wild card matching problem done

--- DP/KnapsackRevision.java
+++ DP/KnapsackRevision.java
@@ -354,9 +354,39 @@ class KnapsackRevision {
         return ans + lcs;
     }
 
+    public static boolean wildcardMatching(String str, String pattern) {
+        int m = str.length();
+        int n = pattern.length();
+
+        boolean dp[][] = new boolean[m+1][n+1];
+        dp[0][0] = true;
+
+        for(int j=1; j<dp.length; j++) {
+            if(pattern.charAt(j-1) == '*') {
+                dp[0][j] = dp[0][j-1];
+            } else {
+                dp[0][j] = false;
+            }
+        }
+
+        for(int i=1; i<m+1; i++) {
+            for(int j=1; j<n+1; j++) {
+                if(str.charAt(i-1) == pattern.charAt(j-1) || pattern.charAt(j-1) == '?') {
+                    dp[i][j] = dp[i-1][j-1];
+                } else if(pattern.charAt(j-1) == '*') {
+                    dp[i][j] = dp[i][j-1] || dp[i-1][j];
+                } else {
+                    dp[i][j] = false;
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+
     public static void main(String[] args) {
-        String str1 = "pear";
-        String str2 = "sea";
-        System.out.println(stringConversion(str1, str2));
+        String str = "baaabab";
+        String pattern = "*****ba*****ab";
+        System.out.println(wildcardMatching(str, pattern));
     }
 }
