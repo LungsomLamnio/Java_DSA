e21837c38828e453045e0c8374263953d977f0c2
LungsomLamnio1004 <lungsomlamnio@gmail.com>
2025-12-09 00:41:57 +0530

    catalan's number using tabulation

--- DP/KnapsackRevision.java
+++ DP/KnapsackRevision.java
@@ -414,10 +414,22 @@ class KnapsackRevision {
         return dp[n] = ans;
     }
 
+    public static int catalanTab(int n, int dp[]) {
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i=2; i<=n; i++) {
+            for(int j=0; j<=i-1; j++) {
+                dp[i] += dp[j] * dp[i-j-1];
+            }
+        }
+
+        return dp[n];
+    }
+
     public static void main(String[] args) {
         int n = 4;
         int dp[] = new int[n+1];
-        Arrays.fill(dp, -1);
-        System.out.println(catalanMemo(n, dp));
+        System.out.println(catalanTab(n, dp));
     }
 }
