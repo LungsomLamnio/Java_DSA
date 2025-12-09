065a9b6f1ae2cef32ad268bd48ff976704838605
LungsomLamnio1004 <lungsomlamnio@gmail.com>
2025-12-09 13:05:26 +0530

    mountain range problem solved using catalan's number

--- DP/KnapsackRevision.java
+++ DP/KnapsackRevision.java
@@ -441,8 +441,23 @@ class KnapsackRevision {
         return dp[n];
     }
 
+    public static int mountainRanges(int n) {
+        int dp[] = new int[n+1];
+        dp[0] = dp[1] = 1;
+
+        for(int i=2; i<=n; i++) {
+            for(int j=0; j<=i-1; j++) {
+                int insideArrangements = dp[j];
+                int outsideArrangements = dp[i-j-1];
+                dp[i] += insideArrangements * outsideArrangements;
+            }
+        }
+
+        return dp[n];
+    }
+
     public static void main(String[] args) {
-        int n = 3;
-        System.out.println(countBST(n));
+        int n = 4;
+        System.out.println(mountainRanges(n));
     }
 }
