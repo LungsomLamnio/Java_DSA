2463935ba39b10bf54a2b68cda3703513d3f6427
LungsomLamnio1004 <lungsomlamnio@gmail.com>
2025-12-09 12:50:26 +0530

    count BST problem solved using catalans number

--- DP/KnapsackRevision.java
+++ DP/KnapsackRevision.java
@@ -427,9 +427,22 @@ class KnapsackRevision {
         return dp[n];
     }
 
-    public static void main(String[] args) {
-        int n = 4;
+    public static int countBST(int n) {
         int dp[] = new int[n+1];
-        System.out.println(catalanTab(n, dp));
+        dp[0] = dp[1] = 1;
+
+        for(int i=2; i<=n; i++) {
+            for(int j=0; j<i; j++) {
+                int leftSubtree = dp[j];
+                int rightSubtree = dp[i-j-1];
+                dp[i] += leftSubtree * rightSubtree;
+            }
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        int n = 3;
+        System.out.println(countBST(n));
     }
 }
