bee7f5033571e990e173c33317f06a8eaf8b45ec
LungsomLamnio1004 <lungsomlamnio@gmail.com>
2025-12-11 12:29:19 +0530

    matrix chain multiplication solved using recursion

--- DP/KnapsackRevision.java
+++ DP/KnapsackRevision.java
@@ -456,8 +456,25 @@ class KnapsackRevision {
         return dp[n];
     }
 
+    public static int mcm(int arr[], int i, int j) {
+        if(i == j) {
+            return 0;
+        }
+
+        int ans = Integer.MAX_VALUE;
+        for(int k=i; k<=j-1; k++) {
+            int cost1 = mcm(arr, i, k);
+            int cost2 = mcm(arr, k+1, j);
+            int cost3 = arr[i-1] * arr[k] * arr[j];
+            int finalCost = cost1 + cost2 + cost3;
+            ans = Math.min(ans, finalCost);
+        }
+
+        return ans;
+    }
+
     public static void main(String[] args) {
-        int n = 4;
-        System.out.println(mountainRanges(n));
+        int arr[] = {1, 2, 3, 4, 3};
+        System.out.println(mcm(arr, 1, arr.length-1));
     }
 }
