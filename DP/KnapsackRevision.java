c1b2c29dce94bc65bf757692b019f1f793354727
LungsomLamnio1004 <lungsomlamnio@gmail.com>
2025-12-13 13:43:09 +0530

    matrix chain multiplication solved using tabulation

--- DP/KnapsackRevision.java
+++ DP/KnapsackRevision.java
@@ -494,14 +494,32 @@ class KnapsackRevision {
         return dp[i][j] = ans;
     }
 
-    public static void main(String[] args) {
-        int arr[] = {1, 2, 3, 4, 3};
+    public static int mcmTab(int arr[]) {
         int n = arr.length;
         int dp[][] = new int[n][n];
-        for(int i=0; i<dp.length; i++) {
-            Arrays.fill(dp[i], -1);
+
+        for(int i=0; i<n; i++) {
+            dp[i][i] = 0;
         }
 
-        System.out.println(mcmMemo(arr, 1, n-1, dp));
+        for(int len=2; len<=n-1; len++) {
+            for(int i=1; i<=len-1; i++) {
+                int j = len + i - 1;
+                dp[i][j] = Integer.MAX_VALUE;
+                for(int k=i; k<=j-1; k++) {
+                    int cost1 = dp[i][k];
+                    int cost2 = dp[k+1][j];
+                    int cost3 = arr[i-1] *arr[k] * arr[j];
+                    dp[i][j] = Math.min(dp[i][i], cost1+cost2+cost3);
+                }
+            }
+        }
+
+        return dp[1][n-1];
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 3};
+        System.out.println(mcmTab(arr));
     }
 }
