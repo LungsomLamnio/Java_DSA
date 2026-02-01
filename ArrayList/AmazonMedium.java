bc31c484ecb9aa14bae5c7b9167c1106ab8436fa
LungsomLamnio1004 <lungsomlamnio@gmail.com>
2026-02-01 22:29:04 +0530

    good days to rob bank is solved

--- ArrayList/AmazonMedium.java
+++ ArrayList/AmazonMedium.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.List;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -37,9 +38,46 @@ public class AmazonMedium {
 
         return count == numCourses;
     }
+
+    public static List<Integer> goodDaysToRobBank(int security[], int time) {
+        int len = security.length;
+        int count = 0;
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        int prefix[] = new int[len];
+        int suffix[] = new int[len];
+
+        for(int i=1; i<len; i++) {
+            if(security[i] <= security[i-1]) { //decreasing
+                count++;
+            } else {
+                count = 0;
+            }
+            prefix[i] = count;
+        }
+
+        count = 0;
+
+        for(int i=len-2; i >= 0; i--) {
+            if(security[i] <= security[i+1]) {
+                count++;
+            } else {
+                count = 0;
+            }
+            suffix[i] = count;
+        }
+
+        for(int i=0; i<len; i++) {
+            if(prefix[i] >= time && suffix[i] >= time) {
+                ans.add(i);
+            }
+        }
+
+        return ans;
+    }
     public static void main(String[] args) {
-        int numCourses = 2;
-        int prerequisites[][] = {{1,0}};
-        System.out.println(courseSchedule(numCourses, prerequisites));
+        int time = 3;
+        int security[] = {0,0,0,0,0};
+        System.out.println(goodDaysToRobBank(security, time));
     }
 }
