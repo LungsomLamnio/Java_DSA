14eefab70219b40b456abad414479d80cde7008f
LungsomLamnio1004 <lungsomlamnio@gmail.com>
2026-02-08 15:09:43 +0530

    racecar problem solved

--- DP/Solution.java
+++ DP/Solution.java
@@ -1,27 +1,30 @@
 class Solution {
-    static int dp[] = new int[10001];
-
+    static int dp[] = new int[1001];
+    
     public static int racecar(int target) {
-        if(dp[target] > 0) return dp[target];
+        if(dp[target] > 0) {
+            return dp[target];
+        }
 
         int k = 32 - Integer.numberOfLeadingZeros(target);
         int res;
 
-        if(target == (1 << k) -1) {
+        if(target == (1 << k) - 1) {
             res = k;
         } else {
+            //overshoot
             res = k + 1 + racecar((1 << k) - 1 - target);
 
-            for(int j = 0; j < k-1; j++) {
-                res = Math.min(res, (k-1) + 2 + j + racecar(target - ((1 << (k-1)) - (1 << j))));
+            //undershoot
+            for(int j=0; j<k-1; j++) {
+                res = Math.min(res, (k-1) + 1 + j + 1 + racecar(target - ((1 << (k-1)) - (1 << j))));
             }
         }
 
         dp[target] = res;
         return res;
     }
-
     public static void main(String[] args) {
-        System.out.println(racecar(6));
+        System.out.println(racecar(5));
     }
 }
